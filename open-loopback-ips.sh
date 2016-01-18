7f2e0fea3bed640d02cfccae9f01732bc04d1b75
Lewis Barnes <lb.barnes@live.co.uk>
2016-01-18 20:20:35 +0000

    Added a script to open commonly used loopback IPs on Mac.

---
+++ open-loopback-ips.sh
@@ -0,0 +1,5 @@
+# Commonly used dev sever IPs normally blocked on Mac.
+ifconfig lo0 alias 127.1.1.1 up
+ifconfig lo0 alias 127.2.2.2 up
+ifconfig lo0 alias 127.3.3.3 up
+ifconfig lo0 alias 127.4.4.4 up
