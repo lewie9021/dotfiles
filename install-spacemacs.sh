c48d3d6a9a676c46784a624f100fc45d037109ce
Lewis Barnes <lb.barnes@live.co.uk>
2016-01-18 20:24:54 +0000

    Modified the script to install dependencies for the Javascript layer.

--- install-spacemacs.sh
+++ install-spacemacs.sh
@@ -1,3 +1,9 @@
+# Setup and download the relevant files.
 cp .spacemacs ~/.spacemacs
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+# Run Emacs to trigger the Spacemacs installation.
 emacs --insecure
+
+# Install dependencies for the JavaScript layer.
+sudo npm install tern jshint -g
